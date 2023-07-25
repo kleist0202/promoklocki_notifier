8d98f8d94605415b0f3890d9f1632aae42d641cb
Przemysław Kleist <pk0202@protonmail.com>
2023-07-25 19:10:31 +0200

    remove unneccessary methods from Cli class

--- promoklocki_notifier/promo_cli.py
+++ promoklocki_notifier/promo_cli.py
@@ -140,35 +140,6 @@ class Cli:
         self.db = db
         self.handler = None
 
-    def format_log(self, log_object: MainDataLog) -> None:
-        print(f"Log id: {log_object.log_id}")
-        print(f"Catalog number: {log_object.catalog_number}")
-        print(f"Name: {log_object.name}")
-        print(f"Link: {log_object.production_link}")
-        print(f"Number of elements: {log_object.number_of_elements}")
-        print(f"Number of minifigures: {log_object.number_of_minifigures}")
-        print(f"Date: {log_object.date}")
-        print(f"Price: {log_object.lowest_price}")
-        print(f"Operation: {log_object.operation}")
-        print(f"Log Timestamp: {log_object.changed_on}")
-        print(f"Accepted?: {log_object.accepted}")
-
-        rows = self.db.get_product_logs(log_object.catalog_number, log_object.changed_on)
-        if len(rows) > 1:
-            latest_log = MainData.create_from_tuple(rows[0][1:-3])
-            second_latest_log = MainData.create_from_tuple(rows[1][1:-3])
-            diff = latest_log.get_differences(second_latest_log)
-            info = print_bold("Changes:\n")
-            for key, val in diff.items():
-                info += f"{key :<20}:  "
-                info += print_gray(f"Old: {val[1]}  ")
-                info += print_warning(f"New: {val[0]}\n")
-            print(f"{info}")
-
-    def get_all_logs(self) -> None:
-        self.rows = self.db.select_not_accepted_logs_reverse()
-        self.logs_number = len(self.rows)
-
     def main_loop(self, arg) -> None:
         if arg:
             self.handler = LogsFormat(self.db)
