984cfce3227373d5e770f2bd8c3aeb6a103027d4
Przemysław Kleist <pk0202@protonmail.com>
2023-08-14 22:17:51 +0200

    set github actions

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,11 +1,13 @@
 from promoklocki_notifier.promo_database import DataBase
 import pytest
+import os
 
 
 @pytest.fixture
 def test_database():
     # Set up a test database and return the DataBase instance with test credentials
-    test_db_credentials = ("promo_database", "promo_manager", "promo", "172.20.0.6", "5432")
+    postgres_ip = os.environ.get("POSTGRES_HOST", "localhost")
+    test_db_credentials = ("promo_database", "promo_manager", "promo", postgres_ip, "5432")
     return DataBase(test_db_credentials)
 
 
